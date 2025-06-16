c4b3340bcbf5d62a83f8ad20a252058ce4d62031
YeisonJara23 <143120812+YeisonJara23@users.noreply.github.com>
2025-06-16 18:05:35 -0500

    Add files via upload

---
+++ particles-config.js
@@ -0,0 +1,53 @@
+particlesJS("particles-js", {
+  particles: {
+    number: {
+      value: 60,
+      density: { enable: true, value_area: 800 }
+    },
+    color: { value: "#ffc107" },
+    shape: {
+      type: "circle",
+      stroke: { width: 0, color: "#000000" },
+      polygon: { nb_sides: 5 }
+    },
+    opacity: {
+      value: 0.3,
+      random: false,
+      anim: { enable: false }
+    },
+    size: {
+      value: 3,
+      random: true,
+      anim: { enable: false }
+    },
+    line_linked: {
+      enable: true,
+      distance: 150,
+      color: "#ffc107",
+      opacity: 0.4,
+      width: 1
+    },
+    move: {
+      enable: true,
+      speed: 1.5,
+      direction: "none",
+      random: false,
+      straight: false,
+      out_mode: "out",
+      bounce: false
+    }
+  },
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onhover: { enable: true, mode: "grab" },
+      onclick: { enable: true, mode: "push" },
+      resize: true
+    },
+    modes: {
+      grab: { distance: 200, line_linked: { opacity: 1 } },
+      push: { particles_nb: 4 }
+    }
+  },
+  retina_detect: true
+});
